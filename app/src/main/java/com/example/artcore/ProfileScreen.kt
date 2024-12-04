a8c48766370a56a7f7c280563240d58dd7c23aa2
Marat <marat.beyshenaliev@gmail.com>
2024-12-04 23:13:45 +0600

    Добавление функционала изменения профля

--- app/src/main/java/com/example/artcore/ProfileScreen.kt
+++ app/src/main/java/com/example/artcore/ProfileScreen.kt
@@ -13,7 +13,12 @@ import androidx.compose.ui.Alignment
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
@@ -23,6 +28,9 @@ import com.google.firebase.storage.FirebaseStorage
 import coil.compose.rememberImagePainter
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 @Composable
 fun ProfileScreen(
@@ -31,6 +39,19 @@ fun ProfileScreen(
     onLogout: () -> Unit
 ) {
     var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var newNickname by remember { mutableStateOf(nickname) }
+    var currentNickname by remember { mutableStateOf(nickname) }  // Новый state для текущего никнейма
+    var isEditingNickname by remember { mutableStateOf(false) }
+    var isChangesPending by remember { mutableStateOf(false) }
+    var showFullAvatar by remember { mutableStateOf(false) }
+    var currentPassword by remember { mutableStateOf("") }
+    var newPassword by remember { mutableStateOf("") }
+    var isPasswordValid by remember { mutableStateOf(false) }
+    var isChangingPassword by remember { mutableStateOf(false) }
+    var isNicknameUnique by remember { mutableStateOf(true) }
+    var nicknameErrorMessage by remember { mutableStateOf("") }
+    var passwordErrorMessage by remember { mutableStateOf("") }
+
     val auth: FirebaseAuth = FirebaseAuth.getInstance()
     val storage: FirebaseStorage = FirebaseStorage.getInstance()
     val database = FirebaseDatabase.getInstance().reference.child("users")
@@ -39,20 +60,14 @@ fun ProfileScreen(
     val uploadImage = { uri: Uri ->
         val userId = auth.currentUser?.uid
         if (userId != null) {
-            // Сначала загружаем изображение в Firebase Storage
             val storageRef = storage.reference.child("profile_images").child(userId)
             storageRef.putFile(uri).addOnSuccessListener {
-                // После успешной загрузки изображения получаем его URL
                 storageRef.downloadUrl.addOnSuccessListener { downloadUri ->
-                    Log.d("ProfileScreen", "Download URL: $downloadUri")
-                    // Сохраняем URL изображения в Firebase Database
-                    database.child(userId).child("profileImageUrl").setValue(downloadUri.toString()).addOnSuccessListener {
-                        Log.d("ProfileScreen", "New profile image URL saved successfully")
-                        // Обновляем imageUri с новым URL
-                        imageUri = downloadUri
-                    }.addOnFailureListener {
-                        Log.e("ProfileScreen", "Error saving new profile image URL: ${it.message}")
-                    }
+                    database.child(userId).child("profileImageUrl").setValue(downloadUri.toString())
+                        .addOnSuccessListener {
+                            imageUri = downloadUri
+                            isChangesPending = true
+                        }
                 }.addOnFailureListener {
                     Log.e("ProfileScreen", "Error getting download URL: ${it.message}")
                 }
@@ -65,14 +80,19 @@ fun ProfileScreen(
     // Получение URL изображения из базы данных
     LaunchedEffect(auth.currentUser?.uid) {
         auth.currentUser?.uid?.let { userId ->
-            // Загружаем URL изображения из Firebase Realtime Database
             database.child(userId).child("profileImageUrl").get().addOnSuccessListener { snapshot ->
                 val imageUrl = snapshot.value as? String
-                Log.d("ProfileScreen", "Fetched profile image URL: $imageUrl")
-                // Если URL существует, устанавливаем его
                 imageUri = imageUrl?.let { Uri.parse(it) }
             }.addOnFailureListener {
                 Log.e("ProfileScreen", "Error fetching profile image URL: ${it.message}")
+            }
+
+            // Получаем актуальный никнейм
+            database.child(userId).child("nickname").get().addOnSuccessListener { snapshot ->
+                currentNickname = snapshot.value as? String ?: ""
+                newNickname = currentNickname  // Обновляем newNickname, чтобы оно отображалось
+            }.addOnFailureListener {
+                Log.e("ProfileScreen", "Error fetching nickname: ${it.message}")
             }
         }
     }
@@ -81,7 +101,7 @@ fun ProfileScreen(
     val pickImage = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         if (uri != null) {
             imageUri = uri
-            uploadImage(uri)  // Загружаем выбранное изображение
+            uploadImage(uri)
         }
     }
 
@@ -91,38 +111,246 @@ fun ProfileScreen(
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Отображение изображения профиля или картинки по умолчанию
+        // Отображение аватара
         val painter: Painter = rememberImagePainter(
-            data = imageUri ?: R.drawable.ic_default_avatar,  // Если imageUri null, используем картинку по умолчанию
+            data = imageUri ?: R.drawable.ic_default_avatar,
             builder = {
                 crossfade(true)
-                memoryCacheKey(imageUri.toString())  // Устанавливаем уникальный ключ для кеша
+                memoryCacheKey(imageUri.toString())
             }
         )
-        Image(
-            painter = painter,
-            contentDescription = "Profile Image",
-            modifier = Modifier
-                .size(120.dp)
-                .clip(CircleShape) // Обрезка изображения в круг
-                .align(Alignment.CenterHorizontally)
-                .background(MaterialTheme.colorScheme.background) // Добавляем фон
-                .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape) // Добавляем границу
-                .aspectRatio(1f), // Устанавливаем соотношение сторон 1:1
-            contentScale = ContentScale.Crop
-        )
+        if (showFullAvatar) {
+            // Показываем аватар в полном размере
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = painter,
+                    contentDescription = "Profile Image",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp),
+                    contentScale = ContentScale.Fit
+                )
+                // Кнопки внутри Box, чтобы они отображались поверх изображения
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(32.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        // Кнопка для изменения аватарки
+                        Button(onClick = { pickImage.launch("image/*") }) {
+                            Text("Изменить аватар")
+                        }
+                        // Кнопка для закрытия полноэкранного режима
+                        Button(onClick = { showFullAvatar = false }) {
+                            Text("Закрыть")
+                        }
+                    }
+                }
+            }
+        } else {
+            // Отображаем аватар в обычном размере
+            Image(
+                painter = painter,
+                contentDescription = "Profile Image",
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                    .clickable { showFullAvatar = true },
+                contentScale = ContentScale.Crop
+            )
+        }
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Email: $email")
-        Text(text = "Nickname: $nickname")
+        // Почта
+        Text(text = "Почта: $email", style = MaterialTheme.typography.bodyMedium)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Отображение текущего никнейма
+        if (isEditingNickname) {
+            OutlinedTextField(
+                value = newNickname,
+                onValueChange = {
+                    newNickname = it
+                    isChangesPending = true
+                },
+                label = { Text("Никнейм") },
+                isError = newNickname.isEmpty(),  // Проверка на пустое поле
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+            )
+
+            if (newNickname.isEmpty()) {
+                Text(text = "Никнейм не может быть пустым!", color = MaterialTheme.colorScheme.error)
+            }
+
+            Row {
+                Button(onClick = {
+                    isEditingNickname = false
+                    newNickname = currentNickname // Возвращаем старое значение, если отменить
+                }) {
+                    Text("Отмена")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = {
+                    if (newNickname.isNotEmpty()) {
+                        val userId = auth.currentUser?.uid
+                        if (userId != null) {
+                            database.child(userId).child("nickname").setValue(newNickname).addOnSuccessListener {
+                                currentNickname = newNickname // Обновляем текущий никнейм
+                                isChangesPending = false
+                                isEditingNickname = false
+                                Log.d("ProfileScreen", "Nickname updated successfully")
+                            }.addOnFailureListener {
+                                Log.e("ProfileScreen", "Error updating nickname: ${it.message}")
+                            }
+                        }
+                    }
+                }) {
+                    Text("Сохранить изменения")
+                }
+            }
+        } else {
+            Text("Никнейм: $currentNickname")  // Отображаем актуальный никнейм
+            Button(onClick = { isEditingNickname = true }) {
+                Text("Редактировать никнейм")
+            }
+        }
+
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        // Кнопка выбора изображения
-        Button(onClick = { pickImage.launch("image/*") }) {
-            Text("Выбрать изображение")
+        // Кнопка для изменения пароля
+        Button(onClick = {
+            isChangingPassword = true
+            currentPassword = "" // Сбрасываем поле для текущего пароля при нажатии на "Изменить пароль"
+        }) {
+            Text("Изменить пароль")
         }
+
+        // Функция для проверки пароля
+        val verifyPassword = { password: String ->
+            if (password.isEmpty()) {
+                passwordErrorMessage = "Поле для пароля не может быть пустым!"
+                isPasswordValid = false
+            } else {
+                val user = auth.currentUser
+                if (user != null) {
+                    auth.signInWithEmailAndPassword(user.email!!, password).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            isPasswordValid = true
+                            isChangingPassword = true
+                            passwordErrorMessage = ""
+                        } else {
+                            isPasswordValid = false
+                            passwordErrorMessage = "Неверный пароль!"
+                        }
+                    }
+                }
+            }
+        }
+
+        // Функция для проверки совпадения паролей
+        val checkPasswordsMatch = { newPassword: String, currentPassword: String ->
+            if (newPassword.isEmpty() || currentPassword.isEmpty()) {
+                passwordErrorMessage = "Пароли не могут быть пустыми."
+                false
+            } else if (newPassword == currentPassword) {
+                passwordErrorMessage = "Новый пароль не должен совпадать с текущим."
+                false
+            } else {
+                passwordErrorMessage = "" // Очистка ошибки, если пароли не совпадают
+                true
+            }
+        }
+
+        // Обновленный блок кода, где происходит изменение пароля
+        if (isChangingPassword) {
+            if (isPasswordValid) {
+                // Поле для нового пароля
+                OutlinedTextField(
+                    value = newPassword,
+                    onValueChange = {
+                        newPassword = it
+                        // Проверка на совпадение паролей при изменении значения нового пароля
+                        if (!checkPasswordsMatch(newPassword, currentPassword)) {
+                            // Если пароли совпадают, ошибка уже будет установлена в passwordErrorMessage
+                        }
+                    },
+                    label = { Text("Новый пароль") },
+                    modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    visualTransformation = PasswordVisualTransformation()
+                )
+
+                // Отображение ошибки, если она есть
+                if (passwordErrorMessage.isNotEmpty()) {
+                    Text(text = passwordErrorMessage, color = MaterialTheme.colorScheme.error)
+                }
+
+                Row {
+                    Button(onClick = {
+                        if (checkPasswordsMatch(newPassword, currentPassword)) {
+                            if (newPassword.isNotEmpty()) {
+                                auth.currentUser?.updatePassword(newPassword)?.addOnSuccessListener {
+                                    Log.d("ProfileScreen", "Password updated successfully")
+                                    isChangingPassword = false
+                                }?.addOnFailureListener {
+                                    Log.e("ProfileScreen", "Error updating password: ${it.message}")
+                                }
+                            } else {
+                                passwordErrorMessage = "Новый пароль не может быть пустым."
+                            }
+                        }
+                    }) {
+                        Text("Изменить пароль")
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = {
+                        // При отмене сбрасываем состояние
+                        isChangingPassword = false
+                        isPasswordValid = false
+                        currentPassword = "" // Очищаем поле текущего пароля
+                        passwordErrorMessage = "" // Очищаем ошибку
+                    }) {
+                        Text("Отмена")
+                    }
+                }
+            } else {
+                // Проверка текущего пароля
+                OutlinedTextField(
+                    value = currentPassword,
+                    onValueChange = { currentPassword = it },
+                    label = { Text("Текущий пароль") },
+                    modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    visualTransformation = PasswordVisualTransformation()
+                )
+                if (passwordErrorMessage.isNotEmpty()) {
+                    Text(text = passwordErrorMessage, color = MaterialTheme.colorScheme.error)
+                }
+                Row {
+                    Button(onClick = { verifyPassword(currentPassword) }) {
+                        Text("Ввод")
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = {
+                        // При отмене сбрасываем состояние
+                        isChangingPassword = false
+                        isPasswordValid = false // Сбрасываем состояние проверки пароля
+                        currentPassword = "" // Очищаем поле для пароля
+                        passwordErrorMessage = "" // Очищаем ошибку
+                    }) {
+                        Text("Отмена")
+                    }
+                }
+            }
+        }
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
